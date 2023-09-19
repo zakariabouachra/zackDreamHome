38ab87e5ae3ed3030e92ab5de73b11bfc4254187
ZAKARIA <zikotato03@gmail.com>
2023-09-19 17:33:10 -0400

    add files via upload

---
+++ prjWebCsZackDreamHomes/GUI/index.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//
+//     Les changements apportés à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+//     le code est régénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace prjWebCsZackDreamHomes.GUI
+{
+
+
+    public partial class index
+    {
+    }
+}
---
+++ prjWebCsZackDreamHomes/GUI/othersPages/proprietes/proprietesC.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace prjWebCsZackDreamHomes.GUI.othersPages.proprietes
+{
+    public partial class proprietesC : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
